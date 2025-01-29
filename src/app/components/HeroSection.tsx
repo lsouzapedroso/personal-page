b1b49d02063d2c5594951661e716174169c4d9af
lsouzapedroso <lsouzapedroso@gmail.com>
2025-01-28 22:31:49 -0300

    fix: replace Next.js Link with anchor tag for external URL

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -42,13 +42,15 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/public#contact"
+              href="#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Hire Me
+              Contact Me
             </Link>
             <Link
-              href=" "
+              href="https://drive.google.com/file/d/1UFxgQd9RwGJLZTyjWMdvJv8CGb8YwJTB/view?usp=drive_link "
+              target="_blank"
+              rel="noopener noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
